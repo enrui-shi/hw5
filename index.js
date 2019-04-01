14929a98cf6c2e685a2120c498acec44b25f3a0e
stone <enrui.shi@stonybrook.edu>
2019-04-01 18:30:45 -0400

    add c

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const cassandra = require('cassandra-driver');
 
 const client = new cassandra.Client({ 
     contactPoints: ['127.0.0.1:9042'],
-    
+    localDataCenter: 'datacenter1',
+    keyspace: 'hw5'
   });
   
   client.connect(function (err) {
