56bf74cf8b37b0424fb6adfa1361b93346cc6f5c
stone <enrui.shi@stonybrook.edu>
2019-04-02 01:39:05 -0400

    add c

--- routers/deposit.js
+++ routers/deposit.js
@@ -3,18 +3,22 @@ var bodyParser = require('body-parser');
 var router = express.Router();
 var path = require('path');
 var request = require('request');
+var fs = require('fs');
 var multer = require('multer');
 var upload = multer({ dest: 'uploads/' })
 
 router.post('/',upload.single('contents'),function(req,res){
     data = req.body;
-    console.log(req.body)
-    console.log('filename:',data);
+    console.log('filename:',data.filename);
     console.log('content:');
     console.log(req.file);
     var client = req.app.locals.client;
     const query = 'INSERT INTO imgs (filename,contents) VALUES (?, ?)';
-    const params = ['a','b'];
+    const params = [data.filename,'b'];
+    fs.readFile(req.file.path, 'utf8', function(err, data) {  
+        if (err) throw err;
+        console.log(data);
+    });
     client.execute(query, params, { prepare: true }, function (err) {
             if(err){
                 console.log(err);
