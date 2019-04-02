190aecc816b7af4c7dac19f0c13b3008caf86b83
stone <enrui.shi@stonybrook.edu>
2019-04-02 01:26:12 -0400

    add c

--- routers/deposit.js
+++ routers/deposit.js
@@ -14,9 +14,9 @@ router.use(express.static('public'));
 router.post('/',upload.fields([]),function(req,res){
     data = req.body;
     console.log(req.body)
-    //console.log('filename:',data.filename);
-    //console.log('content:');
-    //console.log(data.contents);
+    console.log('filename:',data.filename);
+    console.log('content:');
+    console.log(data.contents);
     var client = req.app.locals.client;
     const query = 'INSERT INTO imgs (filename,contents) VALUES (?, ?)';
     const params = ['a','b'];
